1ac2d5143d5ad838178c4f97c5eb9efacd0d2d18
Justin Wei <justin.lj.wei@gmail.com>
2018-08-05 10:45:19 -0700

    added username functionality

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-	console.log('a user connected');
+	console.log('an anonymous user connected');
+	
 	socket.on('disconnect', function() {
+		// we use io here instead of socket, because if socket disconnected
+		// then we no longer have access to it
+		io.emit('user disconnected', "a user disconnected :(");
 		console.log('user disconnected');
 	});
+	
 	socket.on('chat message', function(msg) {
 		io.emit('chat message', msg);
+	});
+
+	socket.on('set nickname', function(name) {
+		// we use io here instead of socket, because if socket disconnected
+		// then we no longer have access to it
+		socket.broadcast.emit('user connected', name);
+		console.log('nickname: ' + name);
 	});
 })
 
