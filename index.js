b609594e84e239d105b96e7900241fbcff0f79a5
Justin Wei <justin.lj.wei@gmail.com>
2018-08-05 22:57:59 -0700

    added online-users functionality, implemented handlebars, little bit of refactor ya know?

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-var app = require('express')();
+var express = require('express');
+var exphbs= require('express-handlebars');
+var app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var online_users = [];
+
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.render('home', {users: online_users});
 });
+
+function disconnect() {
+	io.emit('user disconnected', "a user disconnected :(");
+	// add logic to remove username from list when they disconnect
+	console.log('user disconnected');
+}
+
+function chat_message(msg) {
+	 io.emit('chat message', msg);
+}
+
+function set_nickname(name, socket) {
+	// we use io here instead of socket, because if socket disconnected
+	// then we no longer have access to it
+	online_users.push(name);
+	io.emit('user connected', {
+		name: name,
+		onlineUsers: online_users
+	});
+	console.log('online users: ' + online_users);
+}
 
 io.on('connection', function(socket) {
 	console.log('an anonymous user connected');
 	
-	socket.on('disconnect', function() {
-		// we use io here instead of socket, because if socket disconnected
-		// then we no longer have access to it
-		io.emit('user disconnected', "a user disconnected :(");
-		console.log('user disconnected');
-	});
+	socket.on('disconnect', disconnect);
 	
-	socket.on('chat message', function(msg) {
-		io.emit('chat message', msg);
-	});
+	socket.on('chat message', chat_message);
 
 	socket.on('set nickname', function(name) {
-		// we use io here instead of socket, because if socket disconnected
-		// then we no longer have access to it
-		socket.broadcast.emit('user connected', name);
-		console.log('nickname: ' + name);
+		set_nickname(name, socket);
 	});
-})
+});
 
 http.listen(3000, function() {
 	console.log('listening on *:3000');
